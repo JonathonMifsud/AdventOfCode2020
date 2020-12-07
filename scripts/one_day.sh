9bec85d905b7082822d92c5afc45afce05024997
JonathonMifsud <jonathon.mifsud1@students.mq.edu.au>
2020-12-07 23:28:00 +1100

    one_day finished

--- scripts/one_day.sh
+++ scripts/one_day.sh
@@ -7,7 +7,7 @@ dos2unix ./input/oneday.txt
 my_array=($(cat ./input/oneday.txt))
 declare -p my_array
 
-# here is my first attempt but I realise that this only adds each number to the next and not the other combinations
+# DOESNT WORK, here is my first attempt but I realise that this only adds each number to the next and not the other combinations
 for i in "${!my_array[@]}"; do
     a="${my_array[i]}"
     b="${my_array[i + 1]}"
@@ -17,7 +17,7 @@ for i in "${!my_array[@]}"; do
     fi
 done
 
-# here is the second attempt, messy but it works
+# WORKS, here is the second attempt, messy but it works
 for i in "${!my_array[@]}"; do
     a="${my_array[i]}"
     for j in "${!my_array[@]}"; do
@@ -39,7 +39,7 @@ done
 # In your expense report, what is the product of the three entries that sum to 2020?
 # Using the above example again, the three entries that sum to 2020 are 979, 366, and 675. Multiplying them together produces the answer, 241861950.
 
-# This is a horrible way to do it and made my laptop run hard but yay it works first try!
+# WORKS, This is a horrible way to do it and made my laptop run hard but yay it works first try!
 for i in "${!my_array[@]}"; do
     a="${my_array[i]}"
     for j in "${!my_array[@]}"; do
